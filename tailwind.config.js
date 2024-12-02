bd9b185451c9f8a41363b3589efa1679c8688796
Gerardo Macias <correojerard@gmail.com>
2024-12-02 12:30:15 +0100

    Nuevos estilos

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 export default {
   content: [ "./index.html", "./src/**/*.{vue,js}", ],
   theme: {
+    colors: {
+      'bg-blue': '#483C9E',
+      'bg-purple': '#68088B',
+      'bg-pink': '#BC168D',
+      'bg-black': '#000000',
+    },
     extend: {},
   },
   plugins: [],
