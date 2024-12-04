9a6181955b68b6a8f30c684c3ad1ebb265fb8ce7
SebastienLeger02 <leger02@gmail.com>
2024-12-04 13:33:28 +0100

    New Views - Category - Platform - Game

---
+++ src/views/Category.vue
@@ -0,0 +1,17 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+    name: "Category",
+    data() {
+        
+    }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Platform.vue
@@ -0,0 +1,17 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+    name: "Platform",
+    data() {
+        
+    }
+
+}
+</script>
+
+<style>
+
+</style>
