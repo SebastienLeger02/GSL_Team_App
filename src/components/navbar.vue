e3800289077339f9e0725cc31504719fa0a5e74b
Gerardo Macias <correojerard@gmail.com>
2024-12-05 11:50:39 +0100

    apiStore and carrousel issues resolved

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -73,28 +73,24 @@ export default {
             categories: [],
         };
     },
-    async mounted() {
+    mounted() {
         // Usamos el store de Pinia para obtener los datos
         const gameStore = useApiStore();
 
-        try {
-            // Llamada a la API desde el store - Le pasamos como agumento el nombre del endpoint al cual llamaremos
-            // await gameStore.fetchGames("games");
-            let id = 545;
-            await gameStore.fetchGames(`game?id=${id}`);
+        // Llamada a la API desde el store - Le pasamos como agumento el nombre del endpoint al cual llamaremos
+        gameStore.fetchGames("games");
+        // let id = 545;
+        // await gameStore.fetchGames(`game?id=${id}`);
 
-            // Sincronizamos los datos del store con el estado local del componente
-            // Lo llamamos gameStore porque llamamos al endpoint general de games
-            // Cogemos las plataformas y las categorias para los menus desplegables del navbar
-            console.log("games: ", gameStore.game);
-            // this.platforms = gameStore.platforms;
-            // this.categories = gameStore.categories;
+        // Sincronizamos los datos del store con el estado local del componente
+        // Lo llamamos gameStore porque llamamos al endpoint general de games
+        // Cogemos las plataformas y las categorias para los menus desplegables del navbar
+        // console.log("games: ", gameStore.game);
+        this.platforms = gameStore.platforms;
+        this.categories = gameStore.categories;
 
-            // console.log("platforms: ", this.platforms);
-            // console.log("categorias: ", this.categories);
-        } catch (error) {
-            console.error("Error al obtener los datos desde el store:", error);
-        }
+        console.log("platforms: ", this.platforms);
+        console.log("categorias: ", this.categories);
     },
 };
 </script>
